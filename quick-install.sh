4774f45ab8387a9b9d0bac6b351bf842c12be7f5
Pazella Mutia Reflin <pazellareflin04@gmail.com>
2026-07-13 08:45:08 +0700

    fix:kembalikan ke Repo BPHN

--- quick-install.sh
+++ quick-install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "Mengunduh source code ILDIS dari Pazella21..."
-git clone https://github.com/Pazella21/ildis.git /tmp/ildis-source
+git clone https://github.com/bphndigitalservice/ildis.git /tmp/ildis-source
 cd /tmp/ildis-source
 
 bash install.sh "$@"
